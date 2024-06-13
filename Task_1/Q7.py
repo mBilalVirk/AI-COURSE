7e9a8e14e8fedb08f507ec98ebbc658ceb773407
mBilalVirk <bilalvirk337@gmail.com>
2024-06-13 21:53:34 +0500

    This is message

---
+++ Task_1/Q7.py
@@ -0,0 +1,12 @@
+# Q7: Declare 5 identifiers, assign values, int, float, string, Boolean, complex. Print the data type using ‘Type’ Function.
+
+intIden = 30
+floatIden = 30.234
+StringIden = "This is a String"
+BoolIden = True
+complexIde = 3+3j
+print(type(intIden))
+print(type(floatIden))
+print(type(StringIden))
+print(type(BoolIden))
+print(type(complexIde))
